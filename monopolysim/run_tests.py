ef87a18fea08f2e7de0f891f2b01138ca4cf8e4f
Peter Evans <evans.peter@gmail.com>
2016-04-12 13:41:52 +0100

    Adding a new test case for Tiles.

--- monopolysim/run_tests.py
+++ monopolysim/run_tests.py
@@ -3,13 +3,15 @@
 from unittest import TestLoader, TextTestRunner, TestSuite
 from tests.test_board import BoardTestCase
 from tests.test_player import PlayerTestCase
+from tests.test_tiles import TileTestCase
 
 
 if __name__ == "__main__":
     loader = TestLoader()
     suite = TestSuite((
         loader.loadTestsFromTestCase(BoardTestCase),
-        loader.loadTestsFromTestCase(PlayerTestCase)
+        loader.loadTestsFromTestCase(PlayerTestCase),
+        loader.loadTestsFromTestCase(TileTestCase)
     ))
     runner = TextTestRunner(verbosity=2)
     runner.run(suite)
